f5507870266c28400a11d6dc8bd94a2bf59f1416
Trường Thịnh <thinhntse170224@fpt.edu.vn>
2024-02-01 13:44:43 +0700

    update gallery

--- src/main/java/com/saxnart/Saxnart/entity/GalleryEntity.java
+++ src/main/java/com/saxnart/Saxnart/entity/GalleryEntity.java
@@ -1,34 +1,34 @@
-//package com.saxnart.Saxnart.entity;
-//
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.util.Date;
-//
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Data
-//@Table(name = "Gallery")
-//public class GalleryEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id")
-//    private Long id;
-//
-//    @Column(columnDefinition = "LONGTEXT")
-//    private String image;
-//
-//    @Column
-//    @Temporal(TemporalType.TIMESTAMP)
-//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
-//    private Date createDate;
-//
-//    @Column
-//    private Boolean status;
-//}
+package com.saxnart.Saxnart.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "Gallery")
+public class GalleryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String image;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
+    private Date createDate;
+
+    @Column
+    private Boolean status;
+}
--- src/main/java/com/saxnart/Saxnart/repository/GalleryRepository.java
+++ src/main/java/com/saxnart/Saxnart/repository/GalleryRepository.java
@@ -1,9 +1,9 @@
-//package com.saxnart.Saxnart.repository;
-//
-//import com.saxnart.Saxnart.entity.GalleryEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface GalleryRepository extends JpaRepository<GalleryEntity, Long> {
-//}
+package com.saxnart.Saxnart.repository;
+
+import com.saxnart.Saxnart.entity.GalleryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GalleryRepository extends JpaRepository<GalleryEntity, Long> {
+}
