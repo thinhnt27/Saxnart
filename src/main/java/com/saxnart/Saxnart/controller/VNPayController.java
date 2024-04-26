e1d159d65bd2e4e6dddee55caff16246f44c34cc
Trường Thịnh <thinhntse170224@fpt.edu.vn>
2024-04-26 17:37:56 +0700

    update ip v3

--- src/main/java/com/saxnart/Saxnart/controller/VNPayController.java
+++ src/main/java/com/saxnart/Saxnart/controller/VNPayController.java
@@ -1,4 +1,5 @@
 package com.saxnart.Saxnart.controller;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -65,118 +66,134 @@ public class VNPayController {
 
     @GetMapping("/vnpay-payment")
     public ResponseEntity<ResponeCustom> getPaymentInfor(@RequestParam(value = "vnp_Amount") String vnp_Amount,
-                                                          @RequestParam(value = "vnp_BankCode") String vnp_BankCode,
+                                                         @RequestParam(value = "vnp_BankCode") String vnp_BankCode,
 //                                                          @RequestParam(value = "vnp_BankTranNo") String vnp_BankTranNo,
-                                                          @RequestParam(value = "vnp_CardType") String vnp_CardType,
-                                                          @RequestParam(value = "vnp_OrderInfo") String vnp_OrderInfo,
-                                                          @RequestParam(value = "vnp_PayDate") String vnp_PayDate,
-                                                          @RequestParam(value = "vnp_ResponseCode") String vnp_ResponseCode,
-                                                          @RequestParam(value = "vnp_TmnCode") String vnp_TmnCode,
-                                                          @RequestParam(value = "vnp_TransactionNo") String vnp_TransactionNo,
-                                                          @RequestParam(value = "vnp_TransactionStatus") String vnp_TransactionStatus,
-                                                          @RequestParam(value = "vnp_TxnRef") String vnp_TxnRef,
-                                                          @RequestParam(value = "vnp_SecureHash") String vnp_SecureHash,
+                                                         @RequestParam(value = "vnp_CardType") String vnp_CardType,
+                                                         @RequestParam(value = "vnp_OrderInfo") String vnp_OrderInfo,
+                                                         @RequestParam(value = "vnp_PayDate") String vnp_PayDate,
+                                                         @RequestParam(value = "vnp_ResponseCode") String vnp_ResponseCode,
+                                                         @RequestParam(value = "vnp_TmnCode") String vnp_TmnCode,
+                                                         @RequestParam(value = "vnp_TransactionNo") String vnp_TransactionNo,
+                                                         @RequestParam(value = "vnp_TransactionStatus") String vnp_TransactionStatus,
+                                                         @RequestParam(value = "vnp_TxnRef") String vnp_TxnRef,
+                                                         @RequestParam(value = "vnp_SecureHash") String vnp_SecureHash,
                                                          HttpServletRequest request) {
 
         String headerAddr = request.getHeader("X-Forwarded-For");
-        String remoteAddr = request.getRemoteAddr();
-        System.out.println(headerAddr);
-        System.out.println(remoteAddr);
-        Map fields = new HashMap();
-        try {
-            encodeAndPut(fields, "vnp_Amount", vnp_Amount);
-            encodeAndPut(fields, "vnp_BankCode", vnp_BankCode);
-//            encodeAndPut(fields, "vnp_BankTranNo", vnp_BankTranNo);
-            encodeAndPut(fields, "vnp_CardType", vnp_CardType);
-            encodeAndPut(fields, "vnp_OrderInfo", vnp_OrderInfo);
-            encodeAndPut(fields, "vnp_PayDate", vnp_PayDate);
-            encodeAndPut(fields, "vnp_ResponseCode", vnp_ResponseCode);
-            encodeAndPut(fields, "vnp_TmnCode", vnp_TmnCode);
-            encodeAndPut(fields, "vnp_TransactionNo", vnp_TransactionNo);
-            encodeAndPut(fields, "vnp_TransactionStatus", vnp_TransactionStatus);
-            encodeAndPut(fields, "vnp_TxnRef", vnp_TxnRef);
-            encodeAndPut(fields, "vnp_SecureHash", vnp_SecureHash);
+//        String remoteAddr = request.getRemoteAddr();
+        List<String> ipAddresses = new ArrayList<>();
 
-            if (fields.containsKey("vnp_SecureHashType"))
-            {
-                fields.remove("vnp_SecureHashType");
-            }
-            if (fields.containsKey("vnp_SecureHash"))
-            {
-                fields.remove("vnp_SecureHash");
-            }
+        // Môi trường production
+        ipAddresses.add("113.52.45.78");
+        ipAddresses.add("116.97.245.130");
+        ipAddresses.add("42.118.107.252");
+        ipAddresses.add("113.20.97.250");
+        ipAddresses.add("203.171.19.146");
+        ipAddresses.add("103.220.87.4");
+        ipAddresses.add("103.220.86.4");
+
+        // Địa chỉ 113.160.92.202
+        ipAddresses.add("113.160.92.202");
+        if (ipAddresses.contains(headerAddr)) {
+
+            System.out.println(headerAddr);
+//        System.out.println(remoteAddr);
+
+            Map fields = new HashMap();
+            try {
+                encodeAndPut(fields, "vnp_Amount", vnp_Amount);
+                encodeAndPut(fields, "vnp_BankCode", vnp_BankCode);
+//            encodeAndPut(fields, "vnp_BankTranNo", vnp_BankTranNo);
+                encodeAndPut(fields, "vnp_CardType", vnp_CardType);
+                encodeAndPut(fields, "vnp_OrderInfo", vnp_OrderInfo);
+                encodeAndPut(fields, "vnp_PayDate", vnp_PayDate);
+                encodeAndPut(fields, "vnp_ResponseCode", vnp_ResponseCode);
+                encodeAndPut(fields, "vnp_TmnCode", vnp_TmnCode);
+                encodeAndPut(fields, "vnp_TransactionNo", vnp_TransactionNo);
+                encodeAndPut(fields, "vnp_TransactionStatus", vnp_TransactionStatus);
+                encodeAndPut(fields, "vnp_TxnRef", vnp_TxnRef);
+                encodeAndPut(fields, "vnp_SecureHash", vnp_SecureHash);
+
+                if (fields.containsKey("vnp_SecureHashType")) {
+                    fields.remove("vnp_SecureHashType");
+                }
+                if (fields.containsKey("vnp_SecureHash")) {
+                    fields.remove("vnp_SecureHash");
+                }
 //            fields.remove("vnp_SecureHashType");
 //            fields.remove("vnp_SecureHash");
-            try {
-                String signValue = VNPayConfig.hashAllFields(fields);
-                if (signValue.equals(vnp_SecureHash)) {
-                    boolean checkOrderId = true; // vnp_TxnRef exists in your database
-                    boolean checkAmount = true; // vnp_Amount is valid (Check vnp_Amount VNPAY returns compared to the amount of the code (vnp_TxnRef) in the Your database).
-                    boolean checkOrderStatus = true; // PaymnentStatus = 0 (pending)
-                    Optional<BookingEntity> bookingEntity = bookingRepository.findById(Long.valueOf(vnp_TxnRef));
-                    if (bookingEntity.isPresent()) {
-                        // Đơn hàng tồn tại trong cơ sở dữ liệu
+                try {
+                    String signValue = VNPayConfig.hashAllFields(fields);
+                    if (signValue.equals(vnp_SecureHash)) {
+                        boolean checkOrderId = true; // vnp_TxnRef exists in your database
+                        boolean checkAmount = true; // vnp_Amount is valid (Check vnp_Amount VNPAY returns compared to the amount of the code (vnp_TxnRef) in the Your database).
+                        boolean checkOrderStatus = true; // PaymnentStatus = 0 (pending)
+                        Optional<BookingEntity> bookingEntity = bookingRepository.findById(Long.valueOf(vnp_TxnRef));
+                        if (bookingEntity.isPresent()) {
+                            // Đơn hàng tồn tại trong cơ sở dữ liệu
 //                        checkOrderId = true;
-                        // Kiểm tra trạng thái đơn hàng
-                        if (bookingEntity.get().getIsPayment() == false) {
-                            // Đơn hàng đang ở trạng thái "pending"
-                            checkOrderStatus = true;
+                            // Kiểm tra trạng thái đơn hàng
+                            if (bookingEntity.get().getIsPayment() == false) {
+                                // Đơn hàng đang ở trạng thái "pending"
+                                checkOrderStatus = true;
+                            } else {
+                                // Đơn hàng đã được xác nhận hoặc hủy bỏ
+                                checkOrderStatus = false;
+                            }
+                            double priceChange = Double.valueOf(vnp_Amount).doubleValue();
+                            if (priceChange == bookingEntity.get().getTotalPrice() * 100) {
+                                checkAmount = true;
+                            } else checkAmount = false;
                         } else {
-                            // Đơn hàng đã được xác nhận hoặc hủy bỏ
-                            checkOrderStatus = false;
+                            // Đơn hàng không tồn tại trong cơ sở dữ liệu
+                            checkOrderId = false;
                         }
-                        double priceChange = Double.valueOf(vnp_Amount).doubleValue();
-                        if (priceChange == bookingEntity.get().getTotalPrice() * 100){
-                            checkAmount = true;
-                        }else checkAmount = false;
-                    } else {
-                        // Đơn hàng không tồn tại trong cơ sở dữ liệu
-                        checkOrderId = false;
-                    }
 
-                    if (checkOrderId) {
-                        if (checkAmount) {
-                            if (checkOrderStatus) {
-                                if ("00".equals(vnp_ResponseCode)) {
-                                    // Update PaymnentStatus = 1 into your Database
-                                    bookingService.paymentVNPaySuccess(Long.valueOf(vnp_TxnRef));
-                                    return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("00", "Confirm Success"));
-                                } else {
-                                    bookingEntity.get().setIsPayment(null);
-                                    bookingRepository.save(bookingEntity.get());
-                                    return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("00", "Confirm Success"));
+                        if (checkOrderId) {
+                            if (checkAmount) {
+                                if (checkOrderStatus) {
+                                    if ("00".equals(vnp_ResponseCode)) {
+                                        // Update PaymnentStatus = 1 into your Database
+                                        bookingService.paymentVNPaySuccess(Long.valueOf(vnp_TxnRef));
+                                        return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("00", "Confirm Success"));
+                                    } else {
+                                        bookingEntity.get().setIsPayment(null);
+                                        bookingRepository.save(bookingEntity.get());
+                                        return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("00", "Confirm Success"));
 //                                    // Update PaymnentStatus = 2 into your Database
 //                                    return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("02", "Order already confirmed"));
+                                    }
+                                } else {
+                                    return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("02", "Order already confirmed"));
                                 }
                             } else {
-                                return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("02", "Order already confirmed"));
+                                return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("04", "Invalid Amount"));
                             }
                         } else {
-                            return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("04", "Invalid Amount"));
+                            return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("01", "Order not Found"));
                         }
                     } else {
-                        return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("01", "Order not Found"));
+                        return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("97", "Invalid Checksum"));
                     }
-                } else {
-                    return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("97", "Invalid Checksum"));
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("99", "Unknown error"));
                 }
-            } catch(Exception e) {
-                return ResponseEntity.status(HttpStatus.OK).body(new ResponeCustom("99", "Unknown error"));
-            }
 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponeCustom("failed", e.getMessage()));
-        }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ResponeCustom("failed", e.getMessage()));
+            }
+        } else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ResponeCustom("failed", "ip not whitelist"));
 
     }
 
-    private void encodeAndPut(Map < String, String > fields, String paramName, String paramValue)
+    private void encodeAndPut(Map<String, String> fields, String paramName, String paramValue)
             throws UnsupportedEncodingException {
-            String encodedName = URLEncoder.encode(paramName, StandardCharsets.US_ASCII.toString());
-            String encodedValue = URLEncoder.encode(paramValue, StandardCharsets.US_ASCII.toString());
-            fields.put(encodedName, encodedValue);
+        String encodedName = URLEncoder.encode(paramName, StandardCharsets.US_ASCII.toString());
+        String encodedValue = URLEncoder.encode(paramValue, StandardCharsets.US_ASCII.toString());
+        fields.put(encodedName, encodedValue);
     }
 
 }
